8eb411d69818ec02943d60503351fa7230418c61
Daria <mattveychuk.dasha@gmail.com>
2026-02-09 14:59:49 +0300

    feat: LoginPage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css'
+import LoginPage from './components/LoginPage/LoginPage'
 
 function App() {
-   return (
-      <div className="App" style={{ fontSize: '30px', color: '#15dcb7' }}>
-         hello, sweetheart
-      </div>
-   )
+  return (
+    <div className="App">
+      <LoginPage></LoginPage>
+    </div>
+  )
 }
 
 export default App
