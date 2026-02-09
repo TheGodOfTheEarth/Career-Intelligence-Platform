5036000f6c7fba216cd5ca5ffd9051e6b8a8bfb5
vadzhazha <bura24064@gmail.com>
2026-02-09 14:11:47 +0300

    initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+   return (
+      <div className="App" style={{ fontSize: '30px', color: '#15dcb7' }}>
+         hello, sweetheart
+      </div>
+   )
+}
+
+export default App
